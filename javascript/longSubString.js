f21f073b95fd19aca4add8041a96fce508dbae78
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-12 11:08:30 -0500

    done with assignment

--- javascript/longSubString.js
+++ javascript/longSubString.js
@@ -1,12 +1,25 @@
 function lengthOfLongestSubstring(s) {
-  // type your code here
+	let longest = "";
+
+  for (let i = 0; i < s.length; i++) {
+		let sub = s[i]
+		for (let j = i + 1; !sub.includes(s[j]) && j < s.length; j++) {
+			sub = sub + s[j];
+		}
+		longest = sub.length > longest.length ? sub : longest;
+	}
+
+	console.log(longest);
+	return longest.length;
 }
 
 if (require.main === module) {
 
+	console.log(lengthOfLongestSubstring(""));
+
 // add your own tests in here
-console.log("Expecting: 3");
-console.log("=>", lengthOfLongestSubstring("abcabcbb"));
+// console.log("Expecting: 3");
+// console.log("=>", lengthOfLongestSubstring("abcabcbb"));
 
 }
 
@@ -14,3 +27,11 @@ module.exports = lengthOfLongestSubstring
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/**
+ * Initialize longest variable
+ * For loop through string
+ * In each loop run additional for loop:
+ *	Each time check if character already included in substring
+ * 	build out substring until duplicate hit
+ * 	if string is longer than longest variable, update
+ */
